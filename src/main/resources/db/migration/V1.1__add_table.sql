883fafa12cec28f14e867ca5ea26d5cf8b492e59
dluat <doluat11@gmail.com>
2019-09-11 16:10:39 -0500

    updated

--- src/main/resources/db/migration/V1.1__add_table.sql
+++ src/main/resources/db/migration/V1.1__add_table.sql
@@ -6,16 +6,16 @@ create table if not exists listing_utility (
 	listing_id bigint NOT NULL,
 	utility_id bigint NOT NULL,
 	CONSTRAINT listing_utility_pk PRIMARY KEY (id),
-	CONSTRAINT listing_utility_fk FOREIGN KEY (listingid) REFERENCES listing(id),
-	CONSTRAINT listing_utility_fk_1 FOREIGN KEY (utilityid) REFERENCES utility(id)
+	CONSTRAINT listing_utility_fk FOREIGN KEY (listing_id) REFERENCES listing(id),
+	CONSTRAINT listing_utility_fk_1 FOREIGN KEY (utility_id) REFERENCES utility(id)
 );
 
-create table if not exists listing.listing_image (
+create table if not exists listing_image (
 	id int8 NOT NULL,
 	listing_id int8 NOT NULL,
 	url varchar NOT NULL,
     CONSTRAINT listing_image_pk PRIMARY KEY (id),
-	CONSTRAINT listing_image_fk FOREIGN KEY (listingid) REFERENCES listing(id)
+	CONSTRAINT listing_image_fk FOREIGN KEY (listing_id) REFERENCES listing(id)
 );
 
 ALTER TABLE listing ADD created_by bigint NOT NULL;
