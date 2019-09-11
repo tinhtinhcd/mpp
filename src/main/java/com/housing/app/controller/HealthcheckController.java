44147c864671394a45efbe3b88b5351773db74ef
Minh Le Quang <lequangminh171085@gmail.com>
2019-09-10 21:20:41 -0500

    Implement HealthCheck API and config swagger

---
+++ src/main/java/com/housing/app/controller/HealthcheckController.java
@@ -0,0 +1,24 @@
+package com.housing.app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/health",
+        produces = {"application/json", "application/xml"})
+public class HealthcheckController {
+
+    @GetMapping
+    public ResponseEntity checkHealth() {
+        Map result = new HashMap();
+        result.put("status", "Application is running");
+        return new ResponseEntity(result, HttpStatus.OK);
+
+    }
+}
