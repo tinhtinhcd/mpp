9d39ba3f64863e7fd0c98e7a5fdbd75698ab537c
dluat <doluat11@gmail.com>
2019-09-12 17:24:10 -0500

    Remove repository

--- src/main/java/com/housing/app/repo/ListingImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.housing.app.repo;
-
-import com.housing.app.model.Listing;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ListingImageRepository extends BaseRepository<Listing, Long> {
-}
