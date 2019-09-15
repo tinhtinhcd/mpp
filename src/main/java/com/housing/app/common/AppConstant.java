e43c01cb1612891c2f8066cce715e37240cde23f
Minh Le Quang <lequangminh171085@gmail.com>
2019-09-15 15:53:15 -0500

    fix date pattern

--- src/main/java/com/housing/app/common/AppConstant.java
+++ src/main/java/com/housing/app/common/AppConstant.java
@@ -4,7 +4,7 @@ package com.housing.app.common;
 public class AppConstant {
   public static final long EXPIRATION_TIME = 86400000; // 1 day
   public static final String TOKEN_PREFIX = "Bearer ";
-  public static final String DATE_FORMAT = "yyyy-mm-dd";
+  public static final String DATE_FORMAT = "yyyy-MM-dd";
   public static final String[] SUPPORT_IMAGE_EXTENSIONS = {"image/jpeg", "image/png", "image/gif"};
   public static final int DEFAULT_RADIUS = 10 ; // ten miles
   public static final int DEFAULT_PAGE_SIZE = 10 ; // ten miles
